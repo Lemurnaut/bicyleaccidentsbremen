aacea1ebbac6f2fa7f15a1c4dde765f304e1e24f
Lemurnaut <80747074+Lemurnaut@users.noreply.github.com>
2023-02-02 12:56:58 +0100

    Add files via upload

--- doc.py
+++ doc.py
@@ -1,11 +1,14 @@
-info = 'Es werden ausschließlich Unfälle mit Fahrradbeteiligung angezeigt. Sofern eine Eingrenzung der ' \
+info = 'Es werden ausschließlich Unfälle mit Fahrradbeteiligung angezeigt (IstRad = 1). Sofern eine Eingrenzung der ' \
        'Unfallbeteiligung auf Fahrrad über einen Filter möglich ist, werden die Unfälle angezeigt an denen ' \
        'ausschließlich ein oder mehrere Fahrräder beteiligt waren. Die Daten lassen keine Aussage über die ' \
-       'Anzahl der Unfallbeteiligten zu. '
+       'Anzahl der Unfallbeteiligten zu. Die Ortsteile Borgfeld, Blockland, Seehausen und Strom überschneiden sich' \
+       'mit keinem Stadtteil, sie werden in der Stadtteilliste unter Unbekannt geführt. Drei Unfälle an der ' \
+       'Stuhrer Landstraße und der Varreler Landstraße sind keinem Ortsteil zugeordnet, sie liegen außerhalb der ' \
+       'Landesgrenze. Sie werden in der Ortsteilliste unter Unbekannt geführt.'
 
 dataproc = 'Notwendige und durchgeführte Datenvorverarbeitung zur Darstellung in dieser App:  ' \
-           'Zusammenfassen der einzelnen csv Dateien (2016-2021) aus der Datenquelle. Filter nach Bundesland Bremen ' \
-           '(ULAND = 4) und ' \
+           'Zusammenfassen der einzelnen csv Dateien (2016-2021) aus der Datenquelle. Filtern der Unfälle' \
+           ' nach Bundesland Bremen (ULAND = 4) und ' \
            'ausschließlich Unfällen mit Fahrradbeteiligung (IstRad = 1). Vereinheitlichung von Spaltennamen. ' \
            'Aufarbeitung der Geodaten (Punkt statt Komma, einzelne Spalten für Breiten- und Längengrade). ' \
            'Löschen nicht oder nicht mehr benötigter Daten(OBJECTID, ULAND, UGEMEINDE, UREGBEZ, LINREFX, LINREFY). ' \
